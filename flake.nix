a793f378bd7e9e69b9ec654306203ae5239903df
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-05-01 07:22:30 -0400

    getting started

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "My rust devenv nix flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
