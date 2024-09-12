84a6d3e88cbd93c0da2e5d2d1658c7235ddfae0f
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-09-11 20:44:19 -0400

    nix changes, other stuff

--- flake.nix
+++ flake.nix
@@ -4,15 +4,16 @@
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, cargo2nix, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, cargo2nix, rust-overlay, ... }@inputs:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [cargo2nix.overlays.default];
+            overlays = [ cargo2nix.overlays.default (import rust-overlay) ];
           };
           rustPkgs = pkgs.rustBuilder.makePackageSet {
             rustChannel = "nightly";
@@ -30,7 +31,42 @@
             ];
           };
         in {
-          devShells.default = import ./shell.nix { inherit pkgs; inherit inputs; };
+          # devShells.default = import ./shell.nix { inherit pkgs; inherit inputs; };
+          devShells.default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              clang
+              # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
+              # llvmPackages_18.bintools
+              # rustup
+              egl-wayland
+
+              udev alsa-lib vulkan-loader
+              xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+              libxkbcommon wayland # To use the wayland feature
+
+              libGL
+              vulkan-headers
+              vulkan-loader
+              vulkan-tools
+              vulkan-tools-lunarg
+              vulkan-extension-layer
+              vulkan-validation-layers # don't need them *strictly* but immensely helpful
+              cmake
+
+              wasm-pack
+              nodejs
+
+              # command automation
+              just
+
+              # for the crate graph
+              graphviz
+            ];
+            nativeBuildInputs = with pkgs; [ pkg-config ] ;
+            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
           packages.default = (rustPkgs.workspace.life {}).overrideAttrs (drv: rec {
             buildInputs = drv.buildInputs or [ ] ++ (with pkgs; [
               udev alsa-lib vulkan-loader
